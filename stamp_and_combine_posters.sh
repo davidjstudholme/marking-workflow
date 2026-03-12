36d0f50ccf9270fd517f78ba0fa2e36de1bc553a
David Studholme <d.j.studholme@exeter.ac.uk>
2026-03-12 11:58:38 +0000

    Add files via upload

---
+++ stamp_and_combine_posters.sh
@@ -0,0 +1,21 @@
+echo brew install imagemagick
+echo brew install ghostscript
+
+mkdir -p stamped
+
+mkdir -p stamped
+
+for f in */File\ submissions/*.pdf; do
+  magick -density 600 "$f" \
+    -gravity southeast \
+    -font "/System/Library/Fonts/Supplemental/Arial.ttf" \
+    -pointsize 32 \
+    -fill black \
+    -annotate +40+40 "$(basename "$f")" \
+    "stamped/$(basename "$f")"
+done
+
+
+
+echo pdftk stamped_*.pdf cat output combined_posters.pdf
+
